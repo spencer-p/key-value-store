81cf4ab3c0af1dd4d5f2757a66e4667e93d11f45
Spencer Peterson <spencerjpeterson@gmail.com>
2019-10-18 14:54:10 -0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/spencer-p/cse138
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/kelseyhightower/envconfig v1.4.0
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/spencer-p/cse138/pkg/util"
+
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+)
+
+const (
+	TIMEOUT = 5 * time.Second
+)
+
+type Config struct {
+	Port string `envconfig:"PORT" required:"true"`
+}
+
+func main() {
+	var env Config
+	envconfig.MustProcess("", &env)
+	log.Printf("Configured: %+v\n", env)
+
+	// Create a muxer w/ hw1 routes and corresponding server
+	r := mux.NewRouter()
+	r.Use(util.WithLog)
+	hw1.Route(r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:" + env.Port,
+		ReadTimeout:  TIMEOUT,
+		WriteTimeout: TIMEOUT,
+	}
+
+	// Run the server watching for errors
+	go func() {
+		log.Println("Starting server")
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for signals to stop the server
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown signal received, exiting...")
+
+	srv.Shutdown(context.Background())
+}
