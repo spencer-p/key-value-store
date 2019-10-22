5e674e4b84d790e2e33d91b38565bddc611f0aaf
Husain Adam Askari <haskari@ucsc.edu>
2019-10-21 19:21:26 -0700

    messages get forward, main sends back message, some response messages are weird

--- pkg/follower/handlers.go
+++ pkg/follower/handlers.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+  "io/ioutil"
+  //"encoding/json"
+  "bytes"
 
 	"github.com/gorilla/mux"
 )
@@ -25,7 +28,43 @@ type follower struct {
 
 func (f *follower) indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "My forwarding address is %s", f.addr)
-	// TODO i think use http.NewRequest and f.client.Do
+
+  requestBody, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    log.Println("error")
+    return
+  }
+
+  r.Body = ioutil.NopCloser(bytes.NewReader(requestBody))
+
+  request, err := http.NewRequest(r.Method, "http://" + f.addr.String() + "/kv-store/{key:.*}", bytes.NewBuffer(requestBody))
+
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  request.Header = make(http.Header)
+
+  for key, value := range r.Header {
+      request.Header[key] = value
+  }
+
+  resp, err := f.client.Do(request)
+
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  log.Println(string(body))
+
+  // TODO i think use http.NewRequest and f.client.Do
 }
 
 func Route(r *mux.Router, fwd string) {
