a23db18c80520eb248a16527ada7d960dd0f0d8f
Spencer Peterson <spjpeter@ucsc.edu>
2019-10-21 22:43:23 -0700

    cleaner forwarding

--- pkg/follower/handlers.go
+++ pkg/follower/handlers.go
@@ -2,14 +2,15 @@
 package follower
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"path"
 	"time"
-	//"encoding/json"
-	"bytes"
 
 	"github.com/gorilla/mux"
 )
@@ -18,8 +19,6 @@ const (
 	TIMEOUT = 5 * time.Second
 )
 
-// TODO adam, vineet ?
-
 // follower holds all state that a follower needs to operate.
 type follower struct {
 	client http.Client
@@ -27,58 +26,43 @@ type follower struct {
 }
 
 func (f *follower) indexHandler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "My forwarding address is %s", f.addr)
-
-	params := mux.Vars(r)
-
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println("error")
+		log.Println("Failed to read body:", err)
+		http.Error(w, "Failed to read request", http.StatusInternalServerError)
 		return
 	}
 
-	r.Body = ioutil.NopCloser(bytes.NewReader(requestBody))
+	target := *f.addr
+	target.Path = path.Join(target.Path, r.URL.Path)
 
-	request, err := http.NewRequest(r.Method, "http://"+f.addr.String()+"/kv-store/"+params["key"], bytes.NewBuffer(requestBody))
-
+	request, err := http.NewRequest(r.Method,
+		target.String(),
+		bytes.NewBuffer(requestBody))
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("Failed to make proxy request:", err)
+		http.Error(w, "Failed to make request", http.StatusInternalServerError)
+		return
 	}
 
-	request.Header = make(http.Header)
-
-	for key, value := range r.Header {
-		request.Header[key] = value
-	}
+	request.Header = r.Header.Clone()
 
 	resp, err := f.client.Do(request)
-
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("Failed to do proxy request:", err)
+		// Presumably the leader is down.
+		http.Error(w, "TODO", http.StatusServiceUnavailable)
+		return
 	}
 
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	s := string(body)
-	fmt.Fprintf(w, s)
-	//dec := json.NewDecoder(resp)
-	//w.WriteHeader(s)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//log.Println("Follow received" + string(body))
-
-	// TODO i think use http.NewRequest and f.client.Do
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
 
-func Route(r *mux.Router, fwd string) {
+func Route(r *mux.Router, fwd string) error {
 	addr, err := url.Parse(fwd)
 	if err != nil {
-		// TODO return an error instead of fataling
-		log.Fatalf("Bad forwarding address %q: %v\n", fwd, addr)
+		return fmt.Errorf("Bad forwarding address %q: %v\n", fwd, addr)
 	}
 
 	f := follower{
@@ -88,5 +72,6 @@ func Route(r *mux.Router, fwd string) {
 		addr: addr,
 	}
 
-	r.PathPrefix("/kv-store/{key:.*}").Handler(http.HandlerFunc(f.indexHandler))
+	r.PathPrefix("/").Handler(http.HandlerFunc(f.indexHandler))
+	return nil
 }
