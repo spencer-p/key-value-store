ac3f38ee77b773b13cfbc9c270fce5633a810e3c
unknown <asperw@gmail.com>
2019-11-03 17:56:53 -0800

    Whoops added package

---
+++ pkg/msg/msg.go
@@ -0,0 +1,20 @@
+package msg
+
+const (
+	FailedToParse = "Failed to parse request body"
+	KeyMissing    = "Key is missing"
+	KeyTooLong    = "Key is too long"
+
+	MainFailure = "Main instance is down"
+
+	PutSuccess    = "Added successfully"
+	UpdateSuccess = "Updated successfully"
+	GetSuccess    = "Retrieved successfully"
+	DeleteSuccess = "Deleted successfully"
+
+	FailedToParse = "Failed to parse request body"
+	KeyMissing    = "Key is missing"
+	KeyDNE        = "Key does not exist"
+	KeyTooLong    = "Key is too long"
+	ValueMissing  = "Value is missing"
+)
