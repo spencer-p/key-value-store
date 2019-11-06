bea82971667f8baa35f8e285fedbb509ef8b701a
Spencer Peterson <spjpeter@ucsc.edu>
2019-11-06 12:16:46 -0800

    helper method for csv parsing (#16)

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,8 +1,10 @@
 package util
 
 import (
+	"encoding/csv"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // WithLog wraps an HTTP handler with a log line with the method and path.
@@ -11,4 +13,10 @@ func WithLog(next http.Handler) http.Handler {
 		log.Printf("%s %s\n", r.Method, r.URL)
 		next.ServeHTTP(w, r)
 	})
+}
+
+// CSVToSlice parses a comma separated string into its constituent strings.
+func CSVToSlice(in string) ([]string, error) {
+	reader := csv.NewReader(strings.NewReader(in))
+	return reader.Read()
 }
