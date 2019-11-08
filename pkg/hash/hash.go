302183e9d1b665b3f9cae973028c2c960f821979
Spencer Peterson <spjpeter@ucsc.edu>
2019-11-07 17:56:19 -0800

    resolve divide by zero error

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -7,6 +7,10 @@ import (
 	"sync"
 )
 
+var (
+	ErrNoElements = errors.New("No elements to hash to")
+)
+
 // Modulo implements simple modulo hashing.
 type Modulo struct {
 	elts []string
@@ -21,15 +25,19 @@ func NewModulo() *Modulo {
 	}
 }
 
-// Get returns the address of the node that should store the given key. No error
-// is returned for modulo hashing.
+// Get returns the address of the node that should store the given key.
 func (m *Modulo) Get(key string) (string, error) {
 	m.mtx.Lock()
 	defer m.mtx.Unlock()
 
+	n := uint32(len(m.elts))
+	if n == 0 {
+		return "", ErrNoElements
+	}
+
 	m.fnv.Reset()
 	fmt.Fprintf(m.fnv, key)
-	i := m.fnv.Sum32() % uint32(len(m.elts))
+	i := m.fnv.Sum32() % n
 	return m.elts[i], nil
 
 }
