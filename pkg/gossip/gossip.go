7bfdf093a7114ea54cb3a5807ea62e148e7b6837
Vineet Ramareddi <vramared@ucsc.edu>
2019-11-25 21:05:45 -0800

    created skeleteon for gossip package

---
+++ pkg/gossip/gossip.go
@@ -0,0 +1,39 @@
+package gossip
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/spencer-p/cse138/pkg/store"
+
+	"github.com/gorilla/mux"
+)
+
+type Manager struct {
+	// stuff that the gossip manager needs to gossip
+	store map[string]string
+}
+
+func NewManager(s map[string]string) *Manager {
+	m := &Manager{
+		store: s,
+	}
+}
+
+// gossips to other replicas periodically
+func (m *Manager) Gossip() {
+
+}
+
+// finds stuff in the store to send to other replicas
+func (m *Manager) gossip() {
+
+}
+
+func (m *Manager) Receive(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (m *Manager) Route() {
+
+}
